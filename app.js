0043b94020dd3d4bfef4e82afa2f4ccf30fec8da
Elizabeth <elizabethsamuel1607@gmail.com>
2021-11-01 22:21:08 +0200

    Added buttons instead of a href

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
-let button = document.querySelector("#button")
+function displayMovie(response) {
+  let button5 = document.querySelector("#button");
+  button5.addEventListener("click", sayHello)
+}
+
+function search(movie) {
+  let apiUrl = 'https://swapi.dev/api/films/2';
+  axios.get(apiUrl).then(displayMovie)
+}
+
+search("The Phantom Menace");
+
+
+
+
+let button3 = document.querySelector("#button")
 
 function sayHello() {
     axios.get('https://swapi.dev/api/films/1').then(function(response){
@@ -6,4 +21,14 @@ function sayHello() {
     })
 }
 
-button.addEventListener("click", sayHello)
+button3.addEventListener("click", sayHello)
+
+let button5 = document.querySelector("#button")
+
+function sayHello() {
+    axios.get('https://swapi.dev/api/films/2').then(function(response){
+        console.log(response)
+    })
+}
+
+button5.addEventListener("click", sayHello)
